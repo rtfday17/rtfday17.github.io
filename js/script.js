62f66c10d3aef2ca4113d02304d128540a63dc7e
rtfday17 <rtfday17@gmail.com>
2017-05-22 04:35:46 +0300

    Responsive version

--- js/script.js
+++ js/script.js
@@ -7,4 +7,14 @@ $(function() {
         $('body').animate({scrollTop:destination},1000);
         return false
     })
+    $('.menu-mob').click(function(){
+    	$('.menu-mob').find('.overflow__menu').toggleClass('overflow__menu-open');
+    })
+	$(document).mouseup(function (e) {
+	    if ($('.overflow__menu').hasClass('overflow__menu-open')) {
+		    if ($('.overflow__menu').has(e.target).length === 0){
+		        $('.overflow__menu').removeClass('overflow__menu-open');
+		    }
+	    }
+});
 });
